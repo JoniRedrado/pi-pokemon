c9b8b1545ef321b5afa2aa4fc4f61f7b775ff711
Jonathan Redrado <jredrado@dblandit.com>
2023-11-24 17:00:52 -0300

    getPokemonById

--- api/src/routes/pokemon/getPokemonById.js
+++ api/src/routes/pokemon/getPokemonById.js
@@ -2,4 +2,34 @@ const axios = require('axios')
 
 module.exports = ( req, res ) => {
     
+    const { id } = req.params;
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(({data})=>{
+            const pokemon = {
+                id: data.id,
+                nombre: data.name,
+                imagen: data.sprites.other["official-artwork"].front_default,
+                vida: data.stats[0].base_stat,
+                ataque: data.stats[1].base_stat,
+                defensa: data.stats[2].base_stat,
+                velocidad: data.stats[5].base_stat,
+                altura: data.height,
+                peso: data.weight,
+                tipos: data.types
+            }
+            console.log(pokemon);
+            res.status(200).json(pokemon)
+        })
+
 }
+
+//ID data.id 
+//nombre data.name
+//imagen data.sprites.other["official-artwork"].front_default
+//vida data.stats[0].base_stat
+//ataque data.stats[1].base_stat
+//defensa data.stats[2].base_stat
+//velocidad data.stats[5].base_stat
+//altura data.height
+//peso data.weight
--- api/src/routes/pokemon/pokemonRouter.js
+++ api/src/routes/pokemon/pokemonRouter.js
@@ -2,7 +2,9 @@ const { Router } = require('express');
 const pokemonRouter = Router();
 
 const getPokemons = require('./getPokemons')
+const getPokemonById = require ('./getPokemonById')
 
 pokemonRouter.get('/', getPokemons)
+pokemonRouter.get('/:id', getPokemonById)
 
 module.exports = pokemonRouter;
